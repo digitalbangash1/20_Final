39fc823a8707e84e1069da891818bf584b61c837
Muhammad Bangash <s092512@student.dtu.dk>
2021-01-10 16:04:39 +0100

    Develepment branch created. Software ready to test.

--- src/main/java/Texts.java
+++ src/main/java/Texts.java
@@ -2,6 +2,6 @@ public class Texts {
 
     public static String proeveLykken = "Du er landet på prøv lykken! Tag et chance kort";
     public  static  String goingToJail = "Du skal i Fængsel!";
-    public static String
+   // public static String
 
 }
